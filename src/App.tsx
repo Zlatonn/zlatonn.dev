1646a8660ca4c813ef65ef690630bcc24bbdc628
zlatonn <tonkar2541@hotmail.com>
2025-03-05 12:51:40 +0700

    ✅ edit: implement function scrollToSection
    - create function scrollToSection
    - send function to Hero & About page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+import Navbar from "./components/Navbar";
+import Hero from "./pages/Hero";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+// Register GSAP Plugin
+gsap.registerPlugin(ScrollToPlugin);
+
 function App() {
-  return <div className="text-5xl text-red-500 font-icon">Hello</div>;
+  // Function to handle smooth scrolling to specific sections
+  const scrollToSection = (sectionId: string) => {
+    gsap.to(window, { duration: 1, scrollTo: `#${sectionId}`, ease: "power2.inOut" });
+  };
+
+  return (
+    <main className="relative min-h-screen w-screen">
+      <Navbar onNavigate={scrollToSection} />
+      <section id="home">
+        <Hero onNavigate={scrollToSection} />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </main>
+  );
 }
 
 export default App;
