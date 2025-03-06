cd1c446f07f383cb8192ee4db270c8a9a9e2c2c3
zlatonn <tonkar2541@hotmail.com>
2025-03-06 13:00:40 +0700

    ✅ edit: Implement contact form with react hook form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,31 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-
 import { Label } from "@radix-ui/react-label";
 
+interface Iform {
+  name: string;
+  email: string;
+  message: string;
+}
+
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Iform>({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+  const onSubmit: SubmitHandler<Iform> = (data) => console.log(data);
+
   return (
     <div className="relative min-h-screen w-screen bg-gradient-to-b from-secondary to-primary">
       <div className="py-28 flex flex-col items-center gap-20">
@@ -19,15 +39,19 @@ const Contact = () => {
             </blockquote>
           </div>
 
-          <form className="px-3 max-w-lg flex flex-col gap-5 items-center text-light">
+          <form onSubmit={handleSubmit(onSubmit)} className="px-3 max-w-lg flex flex-col gap-5 items-center text-light">
             <div className="grid w-full items-center gap-1.5">
               <Label htmlFor="name">Name</Label>
               <Input
                 type="text"
                 id="name"
                 placeholder="enter your name..."
+                {...register("name", {
+                  required: "Name is required.",
+                })}
                 className="bg-light/50 placeholder:text-light border-none outline-none"
               />
+              {errors.name && <p className="w-fit mt-1 text-orange-300 text-sm">{errors.name.message}</p>}
             </div>
             <div className="grid w-full items-center gap-1.5">
               <Label htmlFor="email">Email</Label>
@@ -35,19 +59,34 @@ const Contact = () => {
                 type="text"
                 id="email"
                 placeholder="enter your email..."
+                {...register("email", {
+                  required: "Email is required.",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Invalid email format.",
+                  },
+                })}
                 className="bg-light/50 placeholder:text-light border-none outline-none"
               />
+              {errors.email && <p className="w-fit mt-1 text-orange-300 text-sm">{errors.email.message}</p>}
             </div>
             <div className="grid w-full items-center gap-1.5">
               <Label htmlFor="message">Message</Label>
               <Textarea
                 id="message"
                 placeholder="enter your message..."
+                {...register("message", {
+                  required: "Message is required.",
+                })}
                 className="bg-light/50 placeholder:text-light border-none outline-none"
               />
+              {errors.message && <p className="w-fit mt-1 text-orange-300 text-sm">{errors.message.message}</p>}
             </div>
 
-            <Button className="w-full mt-5 bg-accent text-secondary hover:bg-accent hover:-translate-y-1 hover:shadow-md hover:shadow-secondary transition-all duration-500">
+            <Button
+              type="submit"
+              className="w-full mt-5 bg-accent text-secondary hover:bg-accent hover:-translate-y-1 hover:shadow-md hover:shadow-secondary transition-all duration-500"
+            >
               SUBMIT
             </Button>
           </form>
