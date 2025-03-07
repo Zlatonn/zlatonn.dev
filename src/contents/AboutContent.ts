33aad98b761e359cd051cbbcae353b025fcda4a7
zlatonn <tonkar2541@hotmail.com>
2025-03-07 15:47:06 +0700

    ✨ feat: Create about contents

---
+++ src/contents/AboutContent.ts
@@ -0,0 +1,5 @@
+export const paragraphs = [
+  "👋 Hi, I’m Tonkar, a Frontend Developer with a background in Automation Programming. I switched to web development to deepen my skills and focus on user-centered solutions. My expertise includes HTML, CSS, JavaScript, React, and Tailwind.",
+  "💻 I aim to become a Full Stack Developer, starting with frontend to understand web apps’ visual and interactive elements. Through various projects, I’ve gained experience in state management and API integration.",
+  "🚀 I’m passionate about continuous learning and eager to grow by contributing to real-world projects.",
+];
