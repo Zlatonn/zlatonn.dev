ca296444ea6e8e665c01bb3d68203e401651993f
zlatonn <tonkar2541@hotmail.com>
2025-03-07 14:34:37 +0700

    ♻️ refactor: Hero page
    - using reusable components

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,68 +1,31 @@
-import { useRef } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import PageContainer from "@/components/PageContainer";
+import PreText from "@/components/hero/PreText";
+import HeroTitle from "@/components/hero/HeroTitle";
+import HeroButton from "@/components/hero/HeroButton";
+import ScrollDownIndicator from "@/components/hero/ScrollDownIndicator";
 
-import { faArrowDown, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button } from "@/components/ui/button";
+import { preText } from "@/contents/HeroContent";
+import { heroTitle } from "@/contents/HeroContent";
 
-interface Prop {
-  onNavigate: (sectionId: string) => void;
-}
+import { onNavigateProps as Props } from "@/types/common";
 
-const Hero = ({ onNavigate }: Prop) => {
-  // Refer to the down arrow icon for animation
-  const downArrowRef = useRef(null);
-
-  // Apply GSAP animation to the down arrow for bouncing effect
-  useGSAP(() => {
-    gsap.fromTo(
-      downArrowRef.current,
-      {
-        y: -5,
-        opacity: 0,
-      },
-      {
-        y: 5,
-        opacity: 1,
-        ease: "power2.inOut",
-        duration: 2,
-        repeat: -1,
-      }
-    );
-  });
-
+const Hero = ({ onNavigate }: Props) => {
   return (
-    <div className="relative h-screen w-screen bg-gradient-to-b from-secondary to-primary">
-      <div className="max-w-5xl mx-auto pt-36 flex flex-col gap-5 text-center">
-        <div className="text-accent md:text-2xl">welcome to</div>
+    <PageContainer className="bg-gradient-to-b from-secondary to-primary">
+      <div className="h-screen flex flex-col justify-between items-center">
+        {/* Title */}
+        <div className="max-w-5xl pt-36 flex flex-col gap-5 text-center">
+          <PreText text={preText} className="text-accent" />
 
-        <div className="flex flex-col gap-3 text-accent font-display font-bold text-5xl md:text-7xl xl:text-8xl">
-          <div className="flex justify-center">
-            <div>ZLAT</div>
+          <HeroTitle titles={heroTitle} />
 
-            <div>O</div>
-
-            <div>NN'S</div>
-          </div>
-
-          <div className="flex justify-center text-accent font-display">SPACE</div>
+          <HeroButton title="GET MY CV" />
         </div>
 
-        <div className="mt-3 md:mt-5 xl md:my-7">
-          <Button className="rounded-full bg-light text-secondary md:text-lg xl:text-xl hover:bg-gradient-to-r hover:from-light hover:to-accent duration-300">
-            <FontAwesomeIcon icon={faChevronRight} />
-            GET MY CV
-          </Button>
-        </div>
+        {/* Scroll down Indicator*/}
+        <ScrollDownIndicator onNavigate={onNavigate} />
       </div>
-      <div onClick={() => onNavigate("about")} className="absolute w-full bottom-10 text-center text-accent md:text-2xl cursor-pointer">
-        scroll down
-        <span className="pl-3">
-          <FontAwesomeIcon ref={downArrowRef} icon={faArrowDown} />
-        </span>
-      </div>
-    </div>
+    </PageContainer>
   );
 };
 
