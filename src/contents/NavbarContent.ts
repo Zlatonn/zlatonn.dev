db7559ecb1c9d9b1dcc2935391d700e600fd4d65
zlatonn <tonkar2541@hotmail.com>
2025-03-07 14:28:11 +0700

    ✨ feat: Create navbar contents

---
+++ src/contents/NavbarContent.ts
@@ -0,0 +1,5 @@
+// The title to be displayed as the logo in the navbar
+export const logoTitle: string = "Zlatonn";
+
+// List of navigation items to be displayed in the navbar
+export const navList: string[] = ["Home", "About", "Projects", "Contact"];
