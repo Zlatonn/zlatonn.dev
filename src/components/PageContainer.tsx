a386e22d91830967ce195ea91e3777290e959b17
zlatonn <tonkar2541@hotmail.com>
2025-03-12 10:24:28 +0700

    🎨 style: Edit overflow-x hidden at page container component

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,7 +1,7 @@
 import { PageContainerProps as Props } from "@/types/common";
 
 const PageContainer = ({ children, className = "" }: Props) => {
-  return <div className={`relative min-h-screen w-screen ${className}`}>{children}</div>;
+  return <div className={`relative min-h-screen w-screen overflow-x-hidden ${className}`}>{children}</div>;
 };
 
 export default PageContainer;
