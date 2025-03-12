48b84339acbde69d31c1aa3d329169352efe0770
zlatonn <tonkar2541@hotmail.com>
2025-03-12 17:08:08 +0700

    🐛 fix: Eyeblink incorrect layout
    - fix size of Eyeblink wrapper
    - add height for eye ball

--- src/components/about/EyeBlink.tsx
+++ src/components/about/EyeBlink.tsx
@@ -7,19 +7,24 @@ import eyeBallPic from "../../assets/EyeBall.png";
 
 const EyeBlink = () => {
   // Reference to the eye image for animations
-  const eyeRef = useRef(null);
+  const eyeBallRef = useRef(null);
 
   useGSAP(() => {
     // Create a GSAP timeline for the blinking animation
     const blinkTl = gsap.timeline({ repeat: -1, repeatDelay: 5 });
-    blinkTl.to(eyeRef.current, { scaleY: 0, duration: 0.1 });
-    blinkTl.to(eyeRef.current, { scaleY: 1, duration: 0.1 });
+    blinkTl.to(eyeBallRef.current, { scaleY: 0, duration: 0.1 });
+    blinkTl.to(eyeBallRef.current, { scaleY: 1, duration: 0.1 });
   });
 
   return (
-    <div className="relative w-full">
-      <img src={scleraPic} alt="sclera" className="w-full" />
-      <img ref={eyeRef} src={eyeBallPic} alt="full eye" className="absolute w-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+    <div className="relative size-full">
+      <img src={scleraPic} alt="sclera" className="size-full" />
+      <img
+        ref={eyeBallRef}
+        src={eyeBallPic}
+        alt="eye_ball"
+        className="absolute w-1/2 h-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
     </div>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,10 +9,10 @@ const About = () => {
     <PageContainer className="bg-light">
       <div className="max-w-6xl mx-auto flex flex-col pt-24 md:pt-36">
         <div className="flex justify-center items-center gap-5 md:gap-10">
-          <div className="w-24 md:w-32">
+          <div className="size-24 md:size-32">
             <EyeBlink />
           </div>
-          <div className="w-24 md:w-32">
+          <div className="size-24 md:size-32">
             <EyeBlink />
           </div>
         </div>
