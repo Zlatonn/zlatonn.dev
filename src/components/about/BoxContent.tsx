76d437dedc78358981f523e735d44b1700635144
zlatonn <tonkar2541@hotmail.com>
2025-03-17 13:17:05 +0700

    🐛 fix: Box scaling causes layout shift at about page
    - create hidden text
    - box background define height by refer hidden text content
    - add just start end text typing animation config

--- src/components/about/BoxContent.tsx
+++ src/components/about/BoxContent.tsx
@@ -38,11 +38,10 @@ const BoxContent = ({ paragraphs }: Props) => {
     gsap.to(textRef.current, {
       text: paragraphs.join("<br><br>"),
       duration: 20,
-
       scrollTrigger: {
         trigger: textRef.current,
-        start: "center 90%",
-        end: "center top",
+        start: "top-=20px center",
+        end: () => "+=" + (boxRef.current ? boxRef.current.offsetHeight : 0),
         toggleActions: "play pause resume pause",
         onEnter: () => boxAnimation.play(),
         onLeave: () => boxAnimation.pause(),
@@ -54,15 +53,27 @@ const BoxContent = ({ paragraphs }: Props) => {
       },
     });
   });
+
   return (
-    <div>
-      <div className="relative m-8 px-5 pt-5 pb-20 md:m-12 md:px-8 md:pt-8 md:pb-24 lg:px-10 lg:pt-10 lg:pb-28">
-        <div className="relative text-light font-code font-semibold text-left text-sm sm:text-base md:text-xl 2xl:text-2xl z-[1]">
+    <div className="relative m-8 px-5 pt-5 pb-20 md:m-12 md:px-8 md:pt-8 md:pb-24 lg:px-10 lg:pt-10 lg:pb-28">
+      <div className="relative text-light font-code font-semibold text-left text-sm sm:text-base md:text-xl 2xl:text-2xl z-[1]">
+        {/* Hidden text */}
+        <div className="opacity-10">
+          {paragraphs.map((paragraph, i) => (
+            <span key={i}>
+              {paragraph}
+              <br />
+              <br />
+            </span>
+          ))}
+        </div>
+        {/* Display text */}
+        <div className="absolute top-0 left-0">
           <span ref={textRef}></span>
           <span ref={cursorRef}>|</span>
         </div>
-        <div ref={boxRef} className="absolute inset-0 bg-secondary rounded-3xl border-8 border-primary"></div>
       </div>
+      <div ref={boxRef} className="absolute inset-0 bg-secondary rounded-3xl border-8 border-primary shadow-xl"></div>
     </div>
   );
 };
