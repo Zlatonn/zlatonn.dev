5e4a33841454d57c92b3275f909a21b6b8b37937
zlatonn <tonkar2541@hotmail.com>
2025-03-20 03:04:30 +0700

    🎨 style: edit background about page
    - background shape
    - responsive for box content

--- src/components/about/BoxContent.tsx
+++ src/components/about/BoxContent.tsx
@@ -55,7 +55,7 @@ const BoxContent = ({ paragraphs }: Props) => {
   });
 
   return (
-    <div className="relative m-8 px-5 pt-5 pb-20 md:m-12 md:px-8 md:pt-8 md:pb-24 lg:px-10 lg:pt-10 lg:pb-28">
+    <div className="relative p-5 m-8 sm:mx-16 md:mx-24 md:p-8 lg:my-12 lg:mx-28 lg:p-10">
       <div className="relative text-light font-code font-semibold text-left text-sm sm:text-base md:text-xl 2xl:text-2xl z-[1]">
         {/* Hidden text */}
         <div className="opacity-0">
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,8 @@ import { paragraphs } from "@/contents/AboutContent";
 
 const About = () => {
   return (
-    <PageContainer className="bg-light">
-      <div className="max-w-6xl mx-auto flex flex-col py-20 md:py-28">
+    <PageContainer className="bg-light rounded-t-full drop-shadow-2xl shadow-secondary">
+      <div className="max-w-5xl mx-auto flex flex-col pt-20 lg:py-28">
         <div className="flex justify-center items-center gap-5 md:gap-10">
           <div className="size-24 md:size-32">
             <EyeBlink />
