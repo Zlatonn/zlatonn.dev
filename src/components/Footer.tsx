4167cdf46290c499aa56b809129524ed1c2e4df1
zlatonn <tonkar2541@hotmail.com>
2025-04-09 03:17:30 +0700

    ✅ edit: Adjust accessibility score

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <div className="p-3 lg:p-5 xl:p-8 flex justify-between items-center text-light md:absolute md:bottom-0 md:inset-x-0 ">
-      <div className="md:text-lg lg:text-xl xl:text-2xl">© 2025 Zlatonn. All rights reserved.</div>
+    <div className="p-3 lg:p-5 flex justify-between items-center text-light md:absolute md:bottom-0 md:inset-x-0 ">
+      <div className="md:text-lg lg:text-xl ">© 2025 Zlatonn. All rights reserved.</div>
       <div className="flex gap-3 text-2xl md:text-3xl lg:text-4xl">
-        <a href="https://www.linkedin.com/in/pongsakorn-lengklang/" target="_blank" className="hover:text-accent">
+        <a href="https://www.linkedin.com/in/pongsakorn-lengklang/" target="_blank" className="hover:text-accent" aria-label="linkedin">
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
-        <a href="https://github.com/Zlatonn" target="_blank" className="hover:text-accent">
+        <a href="https://github.com/Zlatonn" target="_blank" className="hover:text-accent" aria-label="github">
           <FontAwesomeIcon icon={faGithub} />
         </a>
       </div>
